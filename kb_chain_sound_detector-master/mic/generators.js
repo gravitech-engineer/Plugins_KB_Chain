d76985e726d457063275059c9945302e168c4e16
Nattkarn <nattkarn@gravitechthai.com>
2019-02-06 14:47:47 +0700

    .

--- kb_chain_sound_detector-master/mic/generators.js
+++
@@ -1,5 +0,0 @@
-
-Blockly.JavaScript['mic_get_data'] = function(block) {
-	var code = 'DEV_I2C1.MIC(0, 0x48).getRawValue()';
-	return [code, Blockly.JavaScript.ORDER_NONE];
-};
--- kb_chain_sound_detector-master/mic/msg/en.js
+++
@@ -1,3 +0,0 @@
-
-Blockly.Msg.MIC_GET_DATA_MESSAGE = "Sound Detector    Read raw data";
-Blockly.Msg.MIC_GET_DATA_TOOLTIP = "Read raw data from KB Chain Sound Detector";
